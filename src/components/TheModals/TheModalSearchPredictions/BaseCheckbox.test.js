a4841061735a1e35ce19f50eaa8b208d988d70f4
solohad1n <fox663399@gmail.com>
2023-11-21 18:26:26 +0600

    Test for the checkbox component

---
+++ src/components/TheModals/TheModalSearchPredictions/BaseCheckbox.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/vue'
+import BaseCheckbox from './BaseCheckbox.vue'
+
+test('renders checked base checkbox with label', () => {
+
+  const label = 'Test Label'
+
+  const value = 1
+
+  const options = {
+    props: {
+      id: 'checkbox',
+      value,
+      modelValue: [value, 2, 3]
+    },
+    slots: {
+      default: label
+    }
+  }
+
+  render(BaseCheckbox, options)
+
+  expect(screen.getByLabelText(label).checked).toBe(true)
+})
+
+test('renders unchecked base checkbox', () => {
+
+  render(BaseCheckbox)
+
+  expect(screen.getByRole('checkbox').checked).toBe(false)
+})
