eac9cb4be62d4f8e12752b073152ac02860e137b
solohad1n <fox663399@gmail.com>
2023-11-21 17:17:00 +0600

    test button

--- src/components/TheModals/TheModalSearchPredictions/BaseButton.test.js
+++ src/components/TheModals/TheModalSearchPredictions/BaseButton.test.js
@@ -1,8 +1,15 @@
-import { render } from '@testing-library/vue'
+import { render, screen } from '@testing-library/vue'
 import BaseButton from './BaseButton.vue'
 
 test('renders base button', () => {
-  const { debug } = render(BaseButton)
-
-  debug()
+  // 1. Подготовка перед тестирование // given (arrange)
+  const options = {
+    slots: {
+      default: 'Test Button'
+    }
+  }
+  // 2. Выполняем тот код который мы тестируем // when (act)
+  render(BaseButton, options)
+  // 3. Проверяем совпадает ли действительное с ожидаемым // then (assert)
+  screen.getByText(/test Button/i)
 })
