3c77043534cbaa55e92f7e5dd5b0aa14b41c1203
solohad1n <fox663399@gmail.com>
2023-11-29 13:01:16 +0600

    Testing with snapshots

---
+++ src/components/TheModals/TheModalSearchPredictions/BaseTextareaCounter.test.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/vue'
+import BaseTextareaCounter from './BaseTextareaCounter.vue'
+
+it('renders', () => {
+
+  const options = {
+    slots: {
+      default: '0 / 100'
+    }
+  }
+
+  const { html } = render(BaseTextareaCounter, options)
+
+  expect(html()).toMatchInlineSnapshot(`"<div class=\\"absolute bottom-2 right-2 text-xs text-gray-600 peer-focus:text-black\\">0 / 100</div>"`)
+})
